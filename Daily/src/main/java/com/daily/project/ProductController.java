cb9d16cd197bd2586485b5faf40de269e14e8697
jihogitgit <yoojiho7987@naver.com>
2021-09-05 19:04:01 +0900

    DB 텍스트 파일은 시험삼아 바꾼거니 신경 안쓰셔도 됩니다. 1. ProductMapper insert 오류 수정 values (#{name),#{content},now(),#{image},#{state},#{price1},#{price2},   			#{price3},#{quantity1},#{quantity2},#{kind},0,0)
    => #{name} 중괄호 잘못넣음
    인서트 오류 수정!
    -jiho-

---
+++ Daily/src/main/java/com/daily/project/ProductController.java
@@ -0,0 +1,34 @@
+//package com.daily.project;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import service.ProductService;
+//import vo.ProductVO;
+//
+//
+//@Controller
+//
+//public class ProductController {
+//
+//	@Autowired
+//	ProductService service; // 전역 변수 서비스
+//	
+//	//상품 리스트
+//	@RequestMapping(value = "/pdlist")
+//	public ModelAndView pdlist(ModelAndView mv) {
+//		List<ProductVO> list = service.selectList();
+//		if(list != null ) { 
+//			mv.addObject("product", list); //product에 list를 담는
+//		}else {
+//			mv.addObject("message","현재 상품을 준비중입니다. 조금만 기다려주세요." );
+//		}
+//		mv.setViewName("product_Board/productList");
+//		return mv;
+//	}
+//	
+//}//class
