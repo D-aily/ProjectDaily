e07ab7110a8036e272fadac69dacdad9a04c32e1
plumstudio_ <plumstudio20@gmail.com>
2021-09-13 16:25:26 +0900

    1

--- Daily/src/main/java/service/MemberService.java
+++ Daily/src/main/java/service/MemberService.java
@@ -12,6 +12,7 @@ public interface MemberService {
 	
 	List<MemberVO> selectList();
 	MemberVO selectOne(MemberVO vo);
+	MemberVO findpw(MemberVO vo);
 	int insert(MemberVO vo);
 	int update(MemberVO vo);
 	int delete(MemberVO vo);
