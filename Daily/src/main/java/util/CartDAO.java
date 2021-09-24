806a7294618ef7c639f8668fd3e76bc73dc8f140
jihogitgit <yoojiho7987@naver.com>
2021-09-24 12:57:16 +0900

    2021.09.24 카트부분 수정

--- Daily/src/main/java/util/CartDAO.java
+++ Daily/src/main/java/util/CartDAO.java
@@ -28,8 +28,43 @@ public class CartDAO {
 	}
 	
 	// list 장바구니 목록 
-	public List<CartVO> cselectList(CartVO cvo){
-		return sqlSession.selectList(NS+"selectList",cvo);
+	public List<CartVO> cartList(String id){
+		return sqlSession.selectList(NS+"cartList",id);
 	}
-
+	
+	// 장바구니 전체 삭제
+	public int deleteAll(String id) {
+		return sqlSession.delete(NS+"deleteAll",id);
+	}
+	
+	// 장바구니 개별 삭제
+	public int delete(int cartnum) {
+		return sqlSession.delete(NS+"delete", cartnum);
+	}
+	
+	// 장바구니 업데이트
+	public void update(int cartnum) {
+		
+	}
+	
+	// 장바구니 금액합계
+	public int sumMoney(String id) {
+		return sqlSession.selectOne(NS+"sumMoney", id);
+	}
+	
+	// 장바구니 상품갯수
+	public int countCart(String id, int productnum) {
+		return 0;
+	}
+	
+	// 장바구니 수정
+	public void updateCart(CartVO cvo) {
+		
+	}
+	
+	// 
+	public int modifyCart(CartVO cvo) {
+		return sqlSession.update(NS+"modifyCart", cvo);
+	}
+	
 }//class
--- Daily/src/main/java/vo/CartVO.java
+++ Daily/src/main/java/vo/CartVO.java
@@ -6,11 +6,10 @@ import lombok.Data;
 public class CartVO {
 	private int cartnum; // 장바구니 번호
 	private String id; // 회원 아이디
-	private String productnum; // 상품 넘버
-	private String quantity; // 고른 상품 수량
+	private int productnum; // 상품 넘버
+	private int quantity; // 고른 상품 수량
 	private String date; // 날짜
 	
-	private String mname; // 회원 이름
 	private String pname; // 상품명
 	private int money; // 총금액
 	private int price2; // 상품금액
